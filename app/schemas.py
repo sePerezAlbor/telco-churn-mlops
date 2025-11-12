d17dfb2c9b9f30b611c69bdfe7ff0c3695e83fb9
sePerezAlbor <sebastianperezalbor@outlook.com>
2025-11-12 00:17:14 -0500

    Actualizar comentarios en schemas.py para mejorar la legibilidad pep 8

--- app/schemas.py
+++ app/schemas.py
@@ -1,23 +1,23 @@
 from pydantic import BaseModel
 
+
 class CustomerData(BaseModel):
-    gender: str             #['Female' 'Male']
+    gender: str             # ['Female' 'Male']
     SeniorCitizen: int
-    Partner: str            #['Yes' 'No']
-    Dependents: str         #['Yes' 'No']
+    Partner: str            # ['Yes' 'No']
+    Dependents: str         # ['Yes' 'No']
     tenure: int
-    PhoneService: str       #['Yes' 'No']
-    MultipleLines: str      #['Yes' 'No']
-    InternetService: str    #['DSL' 'Fiber optic' 'No']
-    OnlineSecurity: str     #['Yes' 'No']
-    OnlineBackup: str       #['Yes' 'No']
-    DeviceProtection: str   #['Yes' 'No']
-    TechSupport: str        #['Yes' 'No']
-    StreamingTV: str        #['Yes' 'No']       
-    StreamingMovies: str    #['Yes' 'No']
-    Contract: str           #['Month-to-month' 'One year' 'Two year']
-    PaperlessBilling: str   #['Yes' 'No']
-    PaymentMethod: str      #['Electronic check' 'Mailed check' 'Bank transfer (automatic)' 'Credit card (automatic)']
+    PhoneService: str       # ['Yes' 'No']
+    MultipleLines: str      # ['Yes' 'No']
+    InternetService: str    # ['DSL' 'Fiber optic' 'No']
+    OnlineSecurity: str     # ['Yes' 'No']
+    OnlineBackup: str       # ['Yes' 'No']
+    DeviceProtection: str   # ['Yes' 'No']
+    TechSupport: str        # ['Yes' 'No']
+    StreamingTV: str        # ['Yes' 'No']     
+    StreamingMovies: str    # ['Yes' 'No']
+    Contract: str           # ['Month-to-month' 'One year' 'Two year']
+    PaperlessBilling: str   # ['Yes' 'No']
+    PaymentMethod: str      # ['Electronic check' 'Mailed check' 'Bank transfer (automatic)' 'Credit card (automatic)']
     MonthlyCharges: float
     TotalCharges: float
-
